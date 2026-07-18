375d4f41d478bd101e6f14a4e0c1399e9eb46bfb
marinetaphunya-cpu <marinetaphunya@gmail.com>
2026-07-18 18:19:30 +0700

    Update 5_Static.py

--- pages/5_Static.py
+++ pages/5_Static.py
@@ -68,7 +68,7 @@ try:
             st.plotly_chart(fig_year, use_container_width=True)
 
     else:
-        st.info("ยังไม่มีข้อมูลการประเมินในระบบเจ้า ✨")
+        st.info("ยังไม่มีข้อมูลการประเมินในระบบ ✨")
 
 except Exception as e:
     st.error(f"เกิดข้อผิดพลาดในการดึงข้อมูล: {e}")
