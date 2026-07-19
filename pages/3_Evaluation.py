e3f5b375f70781bf0adcf2afeedae6b9097cf9d1
marinetaphunya-cpu <marinetaphunya@gmail.com>
2026-07-19 10:18:55 +0700

    Update 3_Evaluation.py

--- pages/3_Evaluation.py
+++ pages/3_Evaluation.py
@@ -3,6 +3,9 @@ from datetime import datetime
 from supabase import create_client
 
 st.set_page_config(layout="wide", page_title="Evaluation | iKid Secure")
+if "authenticated" not in st.session_state or not st.session_state.get("authenticated"):
+    st.warning("กรุณาเข้าสู่ระบบก่อน! 🐈‍⬛")
+    st.switch_page("app.py")
 
 # เชื่อมต่อฐานข้อมูล
 @st.cache_resource
