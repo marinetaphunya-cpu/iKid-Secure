74a4e8bfbeb5feed5f1f69eb7bd0b7b9bf1e12e5
marinetaphunya-cpu <marinetaphunya@gmail.com>
2026-07-19 10:18:24 +0700

    Update 2_Profile.py

--- pages/2_Profile.py
+++ pages/2_Profile.py
@@ -5,6 +5,9 @@ from supabase import create_client
 
 # 1. ตั้งค่าหน้าเพจ
 st.set_page_config(layout="wide", page_title="Profile | iKid Secure")
+if "authenticated" not in st.session_state or not st.session_state.get("authenticated"):
+    st.warning("กรุณาเข้าสู่ระบบก่อน! 🐈‍⬛")
+    st.switch_page("app.py")
 
 @st.cache_resource
 def init_supabase():
